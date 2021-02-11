51bf13c603e28df9d5a55ca9049928bf3a63b485
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2021-02-11 16:16:45 -0700

    improving help message

--- check-all.py
+++ check-all.py
@@ -6,36 +6,37 @@ parser = argparse.ArgumentParser(
         usage=f'''{sys.argv[0]} [options]
 
 {'-' * 80}
+
 Long Description:
 
     This tool reads each entry from a `compile_commands.json` database, and runs
     each command after appending the following flags:
         -Xclang -load
-        -Xclang /path/to/tool
+        -Xclang PLUGIN_LIBRARY
         -Xclang -plugin
-        -Xclang <name of plugin>
+        -Xclang PLUGIN_NAME
 
     The tool will log each source file the tool runs on.
 
 {'-' * 80}
 
 ''', formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument('-c', '--compile-commands', required=False, type=str,
+parser.add_argument('-p', '--compile-commands', required=False, type=str,
         help='Path to compilation database')
-parser.add_argument('-t', '--tool-path', required=True, type=str,
-        help='Path to clang tool library')
-parser.add_argument('-p', '--plugin', required=True, type=str,
+parser.add_argument('-l', '--plugin-library', required=True, type=str,
+        help='Path to clang plugin library')
+parser.add_argument('-n', '--plugin-name', required=True, type=str,
         help='Name of clang plugin')
 
 if __name__ == '__main__':
     args = parser.parse_args()
 
     database = args.compile_commands if not args.compile_commands is None \
-            else './compile_commands.json'
+            else 'compile_commands.json'
 
     invoke = lambda x: f'{x} -Xclang -load -Xclang' \
-            f' {os.path.abspath(args.tool_path)}' \
-            f' -Xclang -plugin -Xclang {args.plugin}' \
+            f' {os.path.abspath(args.plugin_library)}' \
+            f' -Xclang -plugin -Xclang {args.plugin_name}' \
             ' -fsyntax-only'
 
     database = json.load(open(database, 'r'))
